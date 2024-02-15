0a602e552721fff0109d43052dfb32e0b290224e
ozioma45 <oziomaegole@gmail.com>
2024-02-15 06:52:36 +0100

    finish the testing project, added an index.html file to collect user input

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,3 @@
-let n = 5;
-
 function factorial(n) {
   let ans = 1;
   if (n < 0) {
@@ -9,7 +7,4 @@ function factorial(n) {
   for (let i = 2; i <= n; i++) ans = ans * i;
   return ans;
 }
-
 module.exports = factorial;
-
-console.log(factorial(5));
