18f6f481a3386115932c4b81022adcd1cba7031e
ozioma45 <oziomaegole@gmail.com>
2024-02-15 05:50:52 +0100

    All test is working perfectly

--- src/factorial.js
+++ src/factorial.js
@@ -2,7 +2,9 @@ let n = 5;
 
 function factorial(n) {
   let ans = 1;
-
+  if (n < 0) {
+    throw new Error("Factorial is not defined for negative numbers.");
+  }
   if (n === 0) return 1;
   for (let i = 2; i <= n; i++) ans = ans * i;
   return ans;
--- src/factorial.test.js
+++ src/factorial.test.js
@@ -7,3 +7,9 @@ test("Give the factorial of a given number.", () => {
 test("Factorial of zero", () => {
   expect(factorial(0)).toBe(1);
 });
+
+test("Factorial of negative number (expecting an error or special value)", () => {
+  expect(() => factorial(-5)).toThrow(
+    "Factorial is not defined for negative numbers."
+  );
+});
